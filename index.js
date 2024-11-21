eebf588017daf90ae6d629b3d8c42171b4e2cfdd
GabrielUrenha <gabr10.gal@gmail.com>
2024-11-20 22:32:45 -0300

    projeto iniciado

---
+++ index.js
@@ -0,0 +1,27 @@
+let fruta = "banana";
+
+switch (fruta) {
+  case "abacaxi":
+    console.log("a fruta é abacaxi.");
+
+    break;
+
+  case "maça":
+    console.log("a fruta é maça.");
+
+    break;
+
+  case "laranja":
+    console.log("a fruta é laranja.");
+
+    break;
+
+  case "morango":
+    console.log("a fruta é morango.");
+
+    break;
+
+  default: {
+    console.log("Não temos essa fruta.");
+  }
+}

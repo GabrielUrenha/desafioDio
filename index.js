189c162c6db41c98eff6aa8d2f562eecb63ca44b
GabrielUrenha <gabr10.gal@gmail.com>
2024-11-21 00:01:18 -0300

    iniciando um novo projeto

--- index.js
+++ index.js
@@ -1,27 +1,24 @@
-let fruta = "maça";
+let nomeHeroi = "Carlinhos";
+let xpHeroi = 10;
+const mensagemRanking = " está no nível: ";
 
-switch (fruta) {
-  case "abacaxi":
-    console.log("a fruta é abacaxi.");
+while (xpHeroi < 10000) {
+  xpHeroi++; //ESTUDAR LAÇOS DE REPETIÇÃO PARA CONCLUIR
+}
+console.log(xpHeroi);
 
-    break;
-
-  case "maça":
-    console.log("a fruta é maça.");
-
-    break;
-
-  case "laranja":
-    console.log("a fruta é laranja.");
-
-    break;
-
-  case "morango":
-    console.log("a fruta é morango.");
-
-    break;
-
-  default: {
-    console.log("Não temos essa fruta.");
-  }
+if (xpHeroi <= 1000) {
+  console.log("O herói de nome " + nomeHeroi + mensagemRanking + "Ferro");
+} else if (xpHeroi > 1000 && xpHeroi <= 2000) {
+  console.log("O herói de nome " + nomeHeroi + mensagemRanking + "Bronze");
+} else if (xpHeroi > 2000 && xpHeroi <= 5000) {
+  console.log("O herói de nome " + nomeHeroi + mensagemRanking + "Prata/Ouro");
+} else if (xpHeroi > 5000 && xpHeroi <= 8000) {
+  console.log("O herói de nome " + nomeHeroi + mensagemRanking + "Platina/Diamante");
+} else if (xpHeroi > 8000 && xpHeroi <= 9000) {
+  console.log("O herói de nome " + nomeHeroi + mensagemRanking + "Ascendente");
+} else if (xpHeroi > 9000 && xpHeroi <= 10000) {
+  console.log("O herói de nome " + nomeHeroi + mensagemRanking + "Imortal");
+} else if (xpHeroi > 10000) {
+  console.log("O herói de nome " + nomeHeroi + mensagemRanking + "Radiante");
 }

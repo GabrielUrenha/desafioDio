d9435bf68a3463b878914d007dcfce54e3e49ff1
GabrielUrenha <gabr10.gal@gmail.com>
2024-11-21 21:56:48 -0300

    Próximo ao fim

--- index.js
+++ index.js
@@ -1,25 +1,22 @@
 let nomeHeroi = "Carlinhos";
-let xpHeroi = 10000;
+let xpHeroi = 5000; //insira a quantidade xp
 const mensagemRanking = " está no nível: ";
 
 for (cont = 1; cont <= xpHeroi; cont++) {
-  console.log(nomeHeroi + " acumulou: " + cont + " de xp!");
-}
-
-if (xpHeroi <= 1000) {
-  console.log("O herói de nome " + nomeHeroi + mensagemRanking + "Ferro");
-} else if (xpHeroi > 1000 && xpHeroi <= 2000) {
-  console.log("O herói de nome " + nomeHeroi + mensagemRanking + "Bronze");
-} else if (xpHeroi > 2000 && xpHeroi <= 5000) {
-  console.log("O herói de nome " + nomeHeroi + mensagemRanking + "Prata/Ouro");
-} else if (xpHeroi > 5000 && xpHeroi <= 8000) {
-  console.log(
-    "O herói de nome " + nomeHeroi + mensagemRanking + "Platina/Diamante"
-  );
-} else if (xpHeroi > 8000 && xpHeroi <= 9000) {
-  console.log("O herói de nome " + nomeHeroi + mensagemRanking + "Ascendente");
-} else if (xpHeroi > 9000 && xpHeroi <= 10000) {
-  console.log("O herói de nome " + nomeHeroi + mensagemRanking + "Imortal");
-} else if (xpHeroi > 10000) {
-  console.log("O herói de nome " + nomeHeroi + mensagemRanking + "Radiante");
+  //cont começa em 1 e enquanto cont for menor ou igual a xpHeroi, cont incrimenta +1
+  if (xpHeroi <= 1000) {
+    console.log("O herói de nome " + nomeHeroi + mensagemRanking + "Ferro");
+  } else if (xpHeroi > 1000 && xpHeroi <= 2000) {
+    console.log("O herói de nome " + nomeHeroi + mensagemRanking + "Bronze");
+  } else if (xpHeroi > 2000 && xpHeroi <= 5000) {
+    console.log("O herói de nome " + nomeHeroi + mensagemRanking + "Prata/Ouro");
+  } else if (xpHeroi > 5000 && xpHeroi <= 8000) {
+    console.log("O herói de nome " + nomeHeroi + mensagemRanking + "Platina/Diamante");
+  } else if (xpHeroi > 8000 && xpHeroi <= 9000) {
+    console.log("O herói de nome " + nomeHeroi + mensagemRanking + "Ascendente");
+  } else if (xpHeroi > 9000 && xpHeroi <= 10000) {
+    console.log("O herói de nome " + nomeHeroi + mensagemRanking + "Imortal");
+  } else if (xpHeroi > 10000) {
+    console.log("O herói de nome " + nomeHeroi + mensagemRanking + "Radiante");
+  }
 }
